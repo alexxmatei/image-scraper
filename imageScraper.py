5c76341127063639af1d35212198914f293eb955
alexxmatei <alexxmatei@gmail.com>
2023-03-20 22:09:36 +0200

    Create function to create folder if doesn't exist

--- imageScraper.py
+++ imageScraper.py
@@ -2,11 +2,13 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
+from utils import mkDirAtCwdIfNoExist
+
 # Replace this URL with the website you want to scrape
-url = ''
+url = 'https://www.alibaba.com/pla/Top-Quality-8-1-2x3-outer_1600166530400.html?mark=google_shopping&biz=pla&searchText=other%20wheels+tires+%20accessories&product_id=1600166530400&pcy=US'
 
 # Replace 'class_name' with the class name of the HTML element that contains the images
-class_name = ''
+class_name = 'product-img-nav-item'
 
 # Send a request to the website and get the HTML response
 response = requests.get(url)
@@ -16,6 +18,8 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 # Find all HTML elements with the specified class name
 elements = soup.find_all(class_=class_name)
+
+folderPath = mkDirAtCwdIfNoExist("scraped_images")
 
 # Loop through all elements and find the image URLs
 for element in elements:
@@ -28,8 +32,6 @@ for element in elements:
             image_url = f'https:{image_url}'
         print(image_url)
         response = requests.get(image_url)
-        filename = os.path.join(
-            os.getcwd(), "scraped_images", os.path.basename(image_url))
-        print(filename)
+        filename = (folderPath + "/" + os.path.basename(image_url))
         with open(filename, 'wb') as f:
             f.write(response.content)
